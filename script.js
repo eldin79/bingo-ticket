d717b54cb08c9259856526fc916d54c41bb585c6
Salah Malik <segmm@hotmail.com>
2018-05-28 18:14:53 +0100

    Added functions to create tickets from ticket string and to display the contents of the tickets array to script.js. createTickets() and drawTickets() called with input arguments in index.html.
    Signed-off-by: Salah Malik <segmm@hotmail.com>

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // script.js
 
-function createTickets() {
+function createTickets(ticketString) {
+	console.log("createTickets()");
+	var numbersPerTicket = 15;
+	var digitsPerNumber  = 2;
+	var digitsPerTicket = numbersPerTicket * digitsPerNumber;
+	var numTickets = ticketString.length / digitsPerTicket;
+	var tickets = [ticketString.length / digitsPerTicket];
+	var start = 0;
+	var end   = digitsPerTicket;
+	var index = 0;
+	for (var i = 0; i < ticketString.length && index < numTickets; ++i) {
+		tickets[index++] = ticketString.substring(start, end);
+		start = end;
+		end += digitsPerTicket;
+	}
+	return tickets;
 }
 
-function drawTickets() {
+function drawTickets(tickets) {
+	console.log("drawTickets()");
+	for (var i = 0; i < tickets.length; ++i)
+		console.log("Ticket[" + i + "]: " + tickets[i]);
 }
