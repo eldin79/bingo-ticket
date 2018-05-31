467153416cf32596f9f8981a2a50d3b414350ac9
Salah Malik <segmm@hotmail.com>
2018-05-31 21:11:18 +0100

    Removed superfluous code from index.html. drawTickets displays ticket numbers on web page and prints row and column indices in the console.
    Signed-off-by: Salah Malik <segmm@hotmail.com>

--- scripts/script.js
+++ scripts/script.js
@@ -26,7 +26,34 @@ function createTickets(ticketString) {
 }
 
 function drawTickets(tickets) {
-	console.log("drawTickets()");
-	for (var i = 0; i < tickets.length; ++i)
+	for (var i = 0; i < tickets.length; ++i) {
 		console.log("Ticket[" + i + "]: " + tickets[i]);
+		var ticket = tickets[i];
+		document.write("Ticket " + (i + 1) + ": " + ticket + "<br>");
+		for (var j = 0; j < ticket.length; ++j) {
+			var numValue = Number(ticket[j]);
+			var indexValue = numValue - 1;
+			var colIndex = Math.floor(indexValue / 10);
+			var rowIndex = Math.floor(indexValue % 10);
+			console.log("numValue: " + numValue + "  " + 
+						"colIndex: " + colIndex + "  " +
+						"rowIndex: " + rowIndex);
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
